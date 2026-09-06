0d23445e8da58eb361d8c79817acdefc35647189
ZitouniNidhal <88096539+ZitouniNidhal@users.noreply.github.com>
2026-09-06 01:40:25 +0100

    chore: add package metadata to setup.py and configure dependabot for automated dependency updates

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import find_packages, setup
 setup(
 	name="biohub-cell-tracking",
 	version="0.1.0",
+	author="ZitouniNidhal",
+	author_email="88096539+ZitouniNidhal@users.noreply.github.com",
 	package_dir={"": "src"},
 	packages=find_packages("src"),
 )
